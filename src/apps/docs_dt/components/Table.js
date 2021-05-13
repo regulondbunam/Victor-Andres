1f487481d83346981f685cc2e2449a03f0f428da
Victor Andaya <48934786+VictorAndaya@users.noreply.github.com>
2021-05-13 16:18:19 -0500

    fix: Added validation if Table component has not data

--- src/apps/docs_dt/components/Table.js
+++ src/apps/docs_dt/components/Table.js
@@ -12,38 +12,42 @@ const Table = (props) => {
   if (error) return <p>Error...</p>;
 
   const Args = FormatDataTable(data);
-  //console.log(Args);
+
   return (
     <div className={TableCSS.table}>
       <h3 className={TableCSS.title}>Arguments</h3>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Type</th>
-            <th>Required</th>
-            <th>Default</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(Args[props.service]).map((args) =>
-            Args[props.service][args].map((service, index) => (
-              <tr key={index}>
-                <td>{args}</td>
-                <td>{service.Descripcion}</td>
-                <td>{service.Tipo}</td>
-                <td>Not required</td>
-                <td>
-                  {service.ValorPorDefault == null
-                    ? "Not defined"
-                    : service.ValorPorDefault}
-                </td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </table>
+      {Object.keys(Args[props.service]).length != 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Type</th>
+              <th>Required</th>
+              <th>Default</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(Args[props.service]).map((args) =>
+              Args[props.service][args].map((service, index) => (
+                <tr key={index}>
+                  <td>{args}</td>
+                  <td>{service.Descripcion}</td>
+                  <td>{service.Tipo}</td>
+                  <td>{service.Necesario}</td>
+                  <td>
+                    {service.ValorPorDefault == null
+                      ? "Not defined"
+                      : service.ValorPorDefault}
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      ) : (
+        <h4 className={TableCSS.text}>This service has no arguments</h4>
+      )}
     </div>
   );
 };
