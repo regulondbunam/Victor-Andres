66bb293d9231d605bc16b9e26fedc06e52822d60
Victor Andaya <48934786+VictorAndaya@users.noreply.github.com>
2021-05-18 14:05:08 -0500

    fix: added property to not display ___typename in queries

--- src/conf/apollo/apollo_client.js
+++ src/conf/apollo/apollo_client.js
@@ -1,11 +1,12 @@
-import ApolloClient from 'apollo-boost';
-import conf from '../apollo.conf.json'
-
+import ApolloClient, { InMemoryCache } from "apollo-boost";
+import conf from "../apollo.conf.json";
 
 // url web service GraphQL
 const client = new ApolloClient({
-    uri: conf.graphQlUrl,
-  });
-
+  uri: conf.graphQlUrl,
+  cache: new InMemoryCache({
+    addTypename: false,
+  }),
+});
 
 export default client;
